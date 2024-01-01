f079b31ecf00ed92710e6994c9f705e970ed125e
choetaeyang <imsun0101@naver.com>
2024-01-01 20:47:32 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/jpabook/japshop/jpaMain.java
@@ -0,0 +1,33 @@
+package jpabook.japshop;
+
+import jpabook.japshop.domain.Order;
+import jpabook.japshop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaMain {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
